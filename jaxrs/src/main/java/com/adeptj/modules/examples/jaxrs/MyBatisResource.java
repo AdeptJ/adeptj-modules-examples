35e0255bfc62aace511118993858c737883f9fa4
Rakesh Kumar <kumar_rakesh@live.in>
2020-05-27 21:01:03 +0530

    used JsonUtil in place of ContextResolver

--- jaxrs/src/main/java/com/adeptj/modules/examples/jaxrs/MyBatisResource.java
+++ jaxrs/src/main/java/com/adeptj/modules/examples/jaxrs/MyBatisResource.java
@@ -1,16 +1,14 @@
 package com.adeptj.modules.examples.jaxrs;
 
+import com.adeptj.modules.commons.utils.JsonUtil;
 import com.adeptj.modules.examples.mybatis.MyBatisUserRepository;
 import com.adeptj.modules.examples.mybatis.UserXmlMapper;
 import com.adeptj.modules.examples.mybatis.domain.User;
 import com.adeptj.modules.jaxrs.core.JaxRSResource;
-import org.jetbrains.annotations.NotNull;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import javax.json.JsonObject;
-import javax.json.bind.Jsonb;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -18,15 +16,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Providers;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 
 @JaxRSResource(name = "MyBatisResource")
 @Path("/mybatis/users")
@@ -55,10 +48,10 @@ public class MyBatisResource {
 
     @Path("/create")
     @POST
+    @Produces(APPLICATION_JSON)
     @Consumes(APPLICATION_JSON)
-    public Response insertUser(@NotNull JsonObject object, @NotNull @Context Providers providers) {
-        ContextResolver<Jsonb> resolver = providers.getContextResolver(Jsonb.class, APPLICATION_JSON_TYPE);
-        User user = resolver.getContext(Jsonb.class).fromJson(object.toString(), User.class);
+    public Response insertUser(String payload) {
+        User user = JsonUtil.deserialize(payload, User.class);
         this.userRepository.insert(UserXmlMapper.class, user);
         return Response.ok(user).build();
     }
@@ -74,13 +67,9 @@ public class MyBatisResource {
     @PUT
     @Produces(APPLICATION_JSON)
     @Consumes(APPLICATION_JSON)
-    public Response updateUser(@PathParam("id") String id, @NotNull JsonObject object) {
-        User user = new User();
+    public Response updateUser(@PathParam("id") String id, String payload) {
+        User user = JsonUtil.deserialize(payload, User.class);
         user.setId(Long.parseLong(id));
-        user.setEmail(object.getString("email"));
-        user.setFirstName(object.getString("firstName"));
-        user.setLastName(object.getString("lastName"));
-        user.setContact(object.getString("contact"));
         this.userRepository.update(UserXmlMapper.class, user);
         return Response.ok(user).build();
     }
