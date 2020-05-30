0fba0df0611a55a4fe31465e31af979445b08c4e
Rakesh Kumar <kumar_rakesh@live.in>
2020-05-30 08:05:30 +0530

    MyBatis example module improvements.

--- mybatis/src/main/java/com/adeptj/modules/examples/mybatis/MyBatisInfoProviderImpl.java
+++ mybatis/src/main/java/com/adeptj/modules/examples/mybatis/MyBatisInfoProviderImpl.java
@@ -3,23 +3,13 @@ package com.adeptj.modules.examples.mybatis;
 import com.adeptj.modules.data.mybatis.MyBatisInfoProvider;
 import org.osgi.service.component.annotations.Component;
 
-import java.util.List;
+import java.util.Set;
 
 @Component
 public class MyBatisInfoProviderImpl implements MyBatisInfoProvider {
 
     @Override
-    public String getMyBatisConfig() {
-        return MyBatisInfoProvider.DEFAULT_MYBATIS_CONFIG;
-    }
-
-    @Override
-    public String getEnvironmentId() {
-        return "development";
-    }
-
-    @Override
-    public List<Class<?>> getMappers() {
-        return List.of(UserAnnotationMapper.class);
+    public Set<Class<?>> getMappers() {
+        return Set.of(UserAnnotationMapper.class);
     }
 }
