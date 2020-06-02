0cb1b7aeb8018fb61402628b7dff16d38e505ec1
Rakesh Kumar <kumar_rakesh@live.in>
2020-06-02 10:12:09 +0530

    DbUtils column mapping.

--- jaxrs/src/main/java/com/adeptj/modules/examples/jaxrs/DbUtilsResource.java
+++ jaxrs/src/main/java/com/adeptj/modules/examples/jaxrs/DbUtilsResource.java
@@ -3,7 +3,10 @@ package com.adeptj.modules.examples.jaxrs;
 import com.adeptj.modules.commons.jdbc.service.DataSourceService;
 import com.adeptj.modules.examples.mybatis.domain.User;
 import com.adeptj.modules.jaxrs.core.JaxRSResource;
+import org.apache.commons.dbutils.BasicRowProcessor;
+import org.apache.commons.dbutils.BeanProcessor;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.RowProcessor;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.jetbrains.annotations.NotNull;
 import org.osgi.service.component.annotations.Activate;
@@ -17,7 +20,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.lang.invoke.MethodHandles;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -38,14 +44,17 @@ public class DbUtilsResource {
     @GET
     @Produces(APPLICATION_JSON)
     public List<User> getUsers() {
-        List<User> users = null;
-        String query = "SELECT u.FIRST_NAME as firstName, u.LAST_NAME as lastName FROM Users u";
+        Map<String, String> columnMapping = new HashMap<>();
+        columnMapping.put("FIRST_NAME", "firstName");
+        columnMapping.put("LAST_NAME", "lastName");
+        columnMapping.put("MOBILE_NO", "contact");
+        RowProcessor rowProcessor = new BasicRowProcessor(new BeanProcessor(columnMapping));
         QueryRunner queryRunner = new QueryRunner(this.dataSourceService.getDataSource());
         try {
-            users = queryRunner.query(query, new BeanListHandler<>(User.class));
+            return queryRunner.query("SELECT * from users", new BeanListHandler<>(User.class, rowProcessor));
         } catch (SQLException ex) {
             LOGGER.error(ex.getMessage(), ex);
         }
-        return users;
+        return new ArrayList<>();
     }
 }
