1883abf328fc1d0a15f602d6a724ea13c494975d
Rakesh Kumar <kumar_rakesh@live.in>
2022-08-25 08:27:11 +0530

    MongoUserRepository annotated with Repository

--- mongodb/src/main/java/com/adeptj/modules/examples/mongodb/MongoUserRepository.java
+++ mongodb/src/main/java/com/adeptj/modules/examples/mongodb/MongoUserRepository.java
@@ -3,8 +3,10 @@ package com.adeptj.modules.examples.mongodb;
 import com.adeptj.modules.data.mongodb.api.AbstractMongoRepository;
 import com.adeptj.modules.data.mongodb.api.DocumentInfo;
 import com.adeptj.modules.data.mongodb.api.MongoRepository;
+import com.adeptj.modules.data.mongodb.api.Repository;
 import org.osgi.service.component.annotations.Component;
 
+@Repository("UserRepository")
 @DocumentInfo(database_name = "AdeptJ", collection_name = "users")
 @Component(service = {MongoUserRepository.class, MongoRepository.class})
 public class MongoUserRepository extends AbstractMongoRepository<User> {
