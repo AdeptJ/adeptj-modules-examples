e65b3f5159c2292c43acd5f2e298731f77b7e704
Rakesh Kumar <kumar_rakesh@live.in>
2023-08-08 16:41:30 +0530

    Referred adeptj-modules-parent v5

--- mybatis/src/main/java/com/adeptj/modules/examples/mybatis/UserAnnotationMapper.java
+++ mybatis/src/main/java/com/adeptj/modules/examples/mybatis/UserAnnotationMapper.java
@@ -1,9 +1,11 @@
 package com.adeptj.modules.examples.mybatis;
 
 import com.adeptj.modules.examples.mybatis.domain.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 
+@Mapper
 public interface UserAnnotationMapper {
 
     @ResultMap("com.adeptj.modules.examples.mybatis.UserXmlMapper.userMap")
