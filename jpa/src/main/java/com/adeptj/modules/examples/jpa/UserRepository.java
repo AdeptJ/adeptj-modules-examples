96445eee5037b1a7aca4479e2d510d22ecb68f07
Rakesh Kumar <kumar_rakesh@live.in>
2024-11-02 06:02:09 +0530

    override getLogger in UserRepository

--- jpa/src/main/java/com/adeptj/modules/examples/jpa/UserRepository.java
+++ jpa/src/main/java/com/adeptj/modules/examples/jpa/UserRepository.java
@@ -4,8 +4,18 @@ import com.adeptj.modules.data.jpa.JpaRepository;
 import com.adeptj.modules.data.jpa.core.AbstractJpaRepository;
 import com.adeptj.modules.examples.jpa.entity.User;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 @Component(service = {JpaRepository.class, UserRepository.class}, immediate = true)
 public class UserRepository extends AbstractJpaRepository<User, Long> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Override
+    protected Logger getLogger() {
+        return LOGGER;
+    }
 }
